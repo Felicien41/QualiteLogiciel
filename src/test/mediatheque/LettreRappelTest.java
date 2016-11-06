bb23565de71150512fc3a12047f163641c4d0ae0
Félicien Catherin <felicien.catherin@gmail.com>
2016-11-06 22:07:45 +0100

    COrrection mineure

--- src/test/mediatheque/LettreRappelTest.java
+++ src/test/mediatheque/LettreRappelTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
  * Created by felicien on 06/11/16.
  */
 public class LettreRappelTest {
-    String nomMedia= "audio";
+    String nomMedia = "audio";
 
     String code = "1523bd";
     String titre = "titre";
@@ -22,27 +22,24 @@ public class LettreRappelTest {
     String annee = "1999";
     Localisation localisation;
     Genre genre;
-    String classification="classification";
+    String classification = "classification";
 
     FicheEmprunt ficheEmprunt;
 
     @Before
     public void setUp() throws Exception {
 
-        Localisation localisation = new Localisation("salle","rayon");
+        Localisation localisation = new Localisation("salle", "rayon");
 
         Genre genre = new Genre("BD");
         int nombrePage = 252;
 
         Mediatheque mediatheque = new Mediatheque("mediatheque");
-        Client client = new Client("Michel", "Michcmic", "ici", new CategorieClient("Lala", 200, 54, 2.5, 1.5, true),10);
-        Audio audio = new Audio(code, localisation, titre, auteur, annee, genre,classification);
+        Client client = new Client("Michel", "Michcmic", "ici", new CategorieClient("Lala", 200, 54, 2.5, 1.5, true), 10);
+        Audio audio = new Audio(code, localisation, titre, auteur, annee, genre, classification);
         audio.metEmpruntable();
 
-        ficheEmprunt= new FicheEmprunt(mediatheque, client, audio);
-    }
-
-
+        ficheEmprunt = new FicheEmprunt(mediatheque, client, audio);
     }
 
 }
